7e1af0762733e1719fddf3714cd017707ab21a82
KeMura-ops <kenti432@yahoo.co.jp>
2021-07-07 16:05:47 +0900

    動的なスロット名

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    <!-- スロットプロパティの省略記法(テンプレートではなくコンポーネントに書く記法)
-    デフォルトのスロットしか存在しない場合にのみ
-    コンポーネントに記述できる(他に名前付きのスロットが存在しないという前提) -->
-    <LikeHeader v-slot="slotProps">
-      <!-- 名前付きではないデフォルトのスロット -->
+    <LikeHeader>
         <p>{{ slotProps }}</p>
         <h2>セガサターン</h2>
         <h3>マッキントッシュプラス</h3>
         <p>リサフランク420/現代のコンピュー</p>
+        <template v-slot:[title]></template> <!-- 動的なスロット名 -->
+        <!-- 以上でdataにあるtitleにアクセスし
+        <template v-slot:hello></template>と同義になる -->
     </LikeHeader>
     <!-- 「props」で指定したものを属性値のように使うことでデータを受けわたせる -->
     <!-- v-bindで動的に表現できる。(親のdataのnumberを指定している)「:」のみでも良い -->
@@ -25,7 +24,8 @@ import LikeHeader from "./components/LikeHeader.vue"
 export default {
   data() {
     return {
-      number: 92
+      number: 92,
+      title: "hello"
     };
   },
   components: { // コンポーネンツオプション(オブジェクト)
