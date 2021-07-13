c2c84dc165bcb85659e5b5d47a18c7f15a79a3e2
KeMura-ops <kenti432@yahoo.co.jp>
2021-07-13 19:09:04 +0900

    カスタムディレクティブの登録

--- src/main.js
+++ src/main.js
@@ -8,6 +8,16 @@ Vue.config.productionTip = false
 // コンポーネントのグローバル登録(全てのVueファイルで使用できるコンポーネント)
 Vue.component('LikeNumber', LikeNumber);
 
+// カスタムディレクティブの登録
+Vue.directive('border', {
+  // 5つの関数を記述する
+  bind() {},
+  inserted() {},
+  update() {},
+  componentUpdated() {},
+  unbind() {}
+});
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
