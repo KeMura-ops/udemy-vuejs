1ca96cea779d0f0683530a69d1a6662deda5fa10
KeMura-ops <kenti432@yahoo.co.jp>
2021-07-14 15:04:59 +0900

    フック関数について

--- src/main.js
+++ src/main.js
@@ -11,11 +11,22 @@ Vue.component('LikeNumber', LikeNumber);
 // カスタムディレクティブの登録
 Vue.directive('border', {
   // 5つの関数を記述する
-  bind() {},
-  inserted() {},
-  update() {},
-  componentUpdated() {},
-  unbind() {}
+  // それぞれ以下のような引数を持つ
+  bind(el, binding, vnode) {
+    // ディレクティブが初めて対象の要素に紐ついた時(初めの一回にのみ呼ばれる)
+  },
+  inserted(el, binding, vnode) {
+    // 親Nodeに挿入された時(実際のDOMに追加された時)
+  },
+  update(el, binding, vnode, oldVnode) {
+    // コンポーネントが更新される度に。子コンポーネントが更新される前。(親に変更があるが、子には影響がない状態)
+  },
+  componentUpdated(el, binding, vnode, oldVnode) {
+    // コンポーネントが更新される度に。子コンポーネントが更新される後。(親のデータなどに変更があって、その子にも変更がある時)
+  },
+  unbind(el, binding, vnode) {
+    // ディレクティブが紐ついている要素から取り除かれた時
+  }
 });
 
 new Vue({
