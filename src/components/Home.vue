a8ecdea4d385a4b6735fa308b961c479e5cd9788
KeMura-ops <kenti432@yahoo.co.jp>
2021-07-15 17:52:01 +0900

    カスタムディレクティブのローカル登録

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,3 +4,22 @@
   <!-- 引数の後に、「.」で修飾子をいくつも付け加えることが出来る -->
   <p v-border:solid.round.shadow="{width: '3px', color: 'brown'}">Home</p>
 </template>
+
+<script>
+// カスタムディレクティブのローカル登録
+export default {
+  directives: {
+    border(el, binding) {
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)";
+      }
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -8,20 +8,6 @@ Vue.config.productionTip = false
 // コンポーネントのグローバル登録(全てのVueファイルで使用できるコンポーネント)
 Vue.component('LikeNumber', LikeNumber);
 
-// カスタムディレクティブの登録
-Vue.directive('border', function(el, binding) {
-  // それぞれのデータをvalueの後に記述する
-  el.style.borderWidth = binding.value.width;
-  el.style.borderColor = binding.value.color;
-  el.style.borderStyle = binding.arg; // カスタムディレクティブの引数の値を取る
-  if (binding.modifiers.round) { // 修飾子はif文で条件分岐し、trueであれば以降の処理が実行される
-    el.style.borderRadius = "0.5rem";
-  }
-  if (binding.modifiers.shadow) { // 「modifiers」を使用することが重要
-    el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)";
-  }
-});
-
 new Vue({
   render: h => h(App)
 }).$mount('#app')
