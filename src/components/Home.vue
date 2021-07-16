4366c894428bf0d11c84866c81fb758b05d096da
KeMura-ops <kenti432@yahoo.co.jp>
2021-07-16 13:42:11 +0900

    カスタムディレクティブにおけるthisの使用上発生するエラーの確認

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,14 +2,22 @@
   <!-- 複数のデータを渡したい時はオブジェクトにして記述する -->
   <!-- :[引数の値]で引数を渡すことが出来る -->
   <!-- 引数の後に、「.」で修飾子をいくつも付け加えることが出来る -->
-  <p v-border:solid.round.shadow="{width: '3px', color: 'brown'}">Home</p>
+  <p v-border:solid.round.shadow="{width: '3px', color: 'brown'}">{{tmpData}}</p>
 </template>
 
 <script>
 // カスタムディレクティブのローカル登録
 export default {
+  // データ関数を用意して、初期値を表示出来るが、
+  // カスタムディレクティブにおいてはthisを用いて値を上書きすることは不可能なので注意する
+  data() {
+    return {
+      tmpData: "新しい日の誕生"
+    };
+  },
   directives: {
     border(el, binding) {
+      // this.tmpData = "こんにちは"  ←エラーになる
       el.style.borderWidth = binding.value.width;
       el.style.borderColor = binding.value.color;
       el.style.borderStyle = binding.arg;
